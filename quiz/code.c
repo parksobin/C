6425072298152482264d7acdf4df30677e14b291
parksobin <128474086+parksobin@users.noreply.github.com>
2023-05-15 12:01:17 +0900

    quiz_update2

--- quiz/code.c
+++ quiz/code.c
@@ -1,27 +1,34 @@
 #include"otaku_quiz.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-   title();
+   
     int game_state = 1;
     int input = 0;
+
     while (game_state)
     {
+        system("cls");
+        title();
         scanf("%d",&input);
         if(input==2)
         {
+            system("cls");
             option();
             while(game_state)
             {
                 scanf("%d",&input);
                 if (input==1)
                 {
+                    system("cls");
                     title();
                     game_state=0;
                 }
                 else if(input==2)
                 {
+                    system("cls");
                     option();
                 }
                 else
