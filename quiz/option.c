42518d76824106286142eabd2e10711746613a3e
parksobin <128474086+parksobin@users.noreply.github.com>
2023-05-15 11:51:48 +0900

    quiz update1:wq

--- quiz/option.c
+++ quiz/option.c
@@ -58,89 +58,88 @@ int option()
                                 screen[b]='y';
                         }
 
-						else if(a==4)
+			else if(a==4)
                         {
                                 screen[b]='-';
                         }	
 					
-						else if(a==7&&b==3)
+			else if(a==7&&b==3)
                         {
                                 screen[b]='l';
                         }
-						else if(a==7&&b==4)
+			else if(a==7&&b==4)
                         {
                                 screen[b]='o';
                         }
-						else if(a==7&&b==5)
+			else if(a==7&&b==5)
                         {
                                 screen[b]='o';
                         }
-						else if(a==7&&b==6)
+			else if(a==7&&b==6)
                         {
                                 screen[b]='k';
                         }
-						else if(a==7&&b==8)
+			else if(a==7&&b==8)
                         {
                                 screen[b]='a';
                         }
-						else if(a==7&&b==9)
+			else if(a==7&&b==9)
                         {
                                 screen[b]='t';
                         }
-						else if(a==7&&b==11)
+			else if(a==7&&b==11)
                         {
                                 screen[b]='t';
                         }
-						else if(a==7&&b==12)
+			else if(a==7&&b==12)
                         {
                                 screen[b]='h';
                         }
-						else if(a==7&&b==13)
+			else if(a==7&&b==13)
                         {
                                 screen[b]='e';
                         }
-						else if(a==7&&b==15)
+			else if(a==7&&b==15)
                         {
                                 screen[b]='q';
                         }
-						else if(a==7&&b==16)
+			else if(a==7&&b==16)
                         {
                                 screen[b]='u';
                         }
-						else if(a==7&&b==17)
+			else if(a==7&&b==17)
                         {
                                 screen[b]='e';
                         }
-						else if(a==7&&b==18)
+			else if(a==7&&b==18)
                         {
                                 screen[b]='s';
                         }
-						else if(a==7&&b==19)
+			else if(a==7&&b==19)
                         {
                                 screen[b]='t';
                         }
-						else if(a==7&&b==20)
+			else if(a==7&&b==20)
                         {
                                 screen[b]='i';
                         }
-						else if(a==7&&b==21)
+			else if(a==7&&b==21)
                         {
                                 screen[b]='o';
                         }
-						else if(a==7&&b==22)
+			else if(a==7&&b==22)
                         {
                                 screen[b]='n';
                         }
-
-						else if(a==8&&b==3)
+			else if(a==8&&b==3)
                         {
                                 screen[b]='a';
                         }
-						else if(a==8&&b==4)
+			else if(a==8&&b==4)
                         {
                                 screen[b]='n';
                         }
-						else if(a==8&&b==5)
+			else if(a==8&&b==5)
                         {
                                 screen[b]='d';
                         }else if(a==8&&b==7)
@@ -150,231 +149,236 @@ int option()
                         {
                                 screen[b]='r';
                         }
-						else if(a==8&&b==9)
+			else if(a==8&&b==9)
                         {
                                 screen[b]='e';
                         }
-						else if(a==8&&b==10)
+			else if(a==8&&b==10)
                         {
                                 screen[b]='s';
                         }
-						else if(a==8&&b==11)
+			else if(a==8&&b==11)
                         {
                                 screen[b]='s';
                         }
-						else if(a==8&&b==13)
+			else if(a==8&&b==13)
                         {
                                 screen[b]='t';
-                        }else if(a==8&&b==14)
+                        }
+                        else if(a==8&&b==14)
                         {
                                 screen[b]='h';
                         }
-						else if(a==8&&b==15)
+			else if(a==8&&b==15)
                         {
                                 screen[b]='e';
                         }
-						else if(a==8&&b==17)
+			else if(a==8&&b==17)
                         {
                                 screen[b]='n';
                         }
-						else if(a==8&&b==18)
+			else if(a==8&&b==18)
                         {
                                 screen[b]='u';
                         }
-						else if(a==8&&b==19)
+			else if(a==8&&b==19)
                         {
                                 screen[b]='m';
                         }
-						else if(a==8&&b==20)
+			else if(a==8&&b==20)
                         {
                                 screen[b]='b';
                         }
-						else if(a==8&&b==21)
+			else if(a==8&&b==21)
                         {
                                 screen[b]='e';
                         }
-						else if(a==8&&b==22)
+			else if(a==8&&b==22)
                         {
                                 screen[b]='r';
                         }
 
-						else if(a==9&&b==3)
+			else if(a==9&&b==3)
                         {
                                 screen[b]='t';
                         }
-						else if(a==9&&b==4)
+			else if(a==9&&b==4)
                         {
                                 screen[b]='o';
                         }
-						else if(a==9&&b==6)
+			else if(a==9&&b==6)
                         {
                                 screen[b]='g';
-                        }else if(a==9&&b==7)
+                        }
+                        else if(a==9&&b==7)
                         {
                                 screen[b]='u';
-                        }else if(a==9&&b==8)
+                        }
+                        else if(a==9&&b==8)
                         {
                                 screen[b]='e';
                         }
-						else if(a==9&&b==9)
+			else if(a==9&&b==9)
                         {
                                 screen[b]='s';
                         }
-						else if(a==9&&b==10)
+			else if(a==9&&b==10)
                         {
                                 screen[b]='s';
                         }
-						else if(a==9&&b==12)
+			else if(a==9&&b==12)
                         {
                                 screen[b]='t';
                         }
-						else if(a==9&&b==13)
+			else if(a==9&&b==13)
                         {
                                 screen[b]='h';
-                        }else if(a==9&&b==14)
+                        }
+                        else if(a==9&&b==14)
                         {
                                 screen[b]='e';
                         }
-						else if(a==9&&b==16)
+			else if(a==9&&b==16)
                         {
                                 screen[b]='a';
                         }
-						else if(a==9&&b==17)
+			else if(a==9&&b==17)
                         {
                                 screen[b]='n';
                         }
-						else if(a==9&&b==18)
+			else if(a==9&&b==18)
                         {
                                 screen[b]='s';
                         }
-						else if(a==9&&b==19)
+			else if(a==9&&b==19)
                         {
                                 screen[b]='w';
                         }
-						else if(a==9&&b==20)
+			else if(a==9&&b==20)
                         {
                                 screen[b]='e';
                         }
-						else if(a==9&&b==21)
+			else if(a==9&&b==21)
                         {
                                 screen[b]='r';
                         }
-						else if(a==9&&b==22)
+			else if(a==9&&b==22)
                         {
                                 screen[b]='.';
                         }
-
-						else if(a==10&&b==3)
+			else if(a==10&&b==3)
                         {
                                 screen[b]='b';
                         }
-						else if(a==10&&b==4)
+			else if(a==10&&b==4)
                         {
                                 screen[b]='e';
                         }
-						else if(a==10&&b==6)
+			else if(a==10&&b==6)
                         {
                                 screen[b]='t';
-                        }else if(a==10&&b==7)
+                        }
+                        else if(a==10&&b==7)
                         {
                                 screen[b]='h';
-                        }else if(a==10&&b==8)
+                        }
+                        else if(a==10&&b==8)
                         {
                                 screen[b]='e';
                         }
-						else if(a==10&&b==10)
+			else if(a==10&&b==10)
                         {
                                 screen[b]='b';
                         }
-						else if(a==10&&b==11)
+			else if(a==10&&b==11)
                         {
                                 screen[b]='e';
                         }
-						else if(a==10&&b==12)
+			else if(a==10&&b==12)
                         {
                                 screen[b]='s';
-                        }else if(a==10&&b==13)
+                        }
+                        else if(a==10&&b==13)
                         {
                                 screen[b]='t';
                         }
-						else if(a==10&&b==15)
+			else if(a==10&&b==15)
                         {
                                 screen[b]='O';
                         }
-						else if(a==10&&b==16)
+			else if(a==10&&b==16)
                         {
                                 screen[b]='T';
                         }				
-						else if(a==10&&b==17)
+			else if(a==10&&b==17)
                         {
                                 screen[b]='A';
                         }
-						else if(a==10&&b==18)
+			else if(a==10&&b==18)
                         {
                                 screen[b]='K';
                         }
-						else if(a==10&&b==19)
+			else if(a==10&&b==19)
                         {
                                 screen[b]='U';
                         }
-						else if(a==10&&b==20)
+			else if(a==10&&b==20)
                         {
                                 screen[b]='!';
                         }
-						else if(a==10&&b==21)
+			else if(a==10&&b==21)
                         {
                                 screen[b]='!';
                         }
-						else if(a==10&&b==22)
+			else if(a==10&&b==22)
                         {
                                 screen[b]='!';
                         }
-
-                        			else if(a==13&&b==6)
+			else if(a==13&&b==6)
                         {
                                 screen[b]='r';
                         }
-                        			else if(a==13&&b==7)
+			else if(a==13&&b==7)
                         {
                                 screen[b]='e';
                         }
-                        			else if(a==13&&b==8)
+			else if(a==13&&b==8)
                         {
                                 screen[b]='t';
                         }
-                        			else if(a==13&&b==9)
+			else if(a==13&&b==9)
                         {
                                 screen[b]='u';
                         }
-                        			else if(a==13&&b==10)
+ 			else if(a==13&&b==10)
                         {
                                 screen[b]='r';
                         }
-                        			else if(a==13&&b==11)
+			else if(a==13&&b==11)
                         {
                                 screen[b]='n';
                         }
-                        			else if(a==13&&b==13)
+			else if(a==13&&b==13)
                         {
                                 screen[b]='t';
                         }
-                        			else if(a==13&&b==14)
+			else if(a==13&&b==14)
                         {
                                 screen[b]='o';
                         }
-                        			else if(a==13&&b==16)
+			else if(a==13&&b==16)
                         {
                                 screen[b]='m';
                         }
-                        			else if(a==13&&b==17)
+			else if(a==13&&b==17)
                         {
                                 screen[b]='e';
                         }
-                        			else if(a==13&&b==18)
+ 			else if(a==13&&b==18)
                         {
                                 screen[b]='n';
                         }
-                        			else if(a==13&&b==19)
+                        else if(a==13&&b==19)
                         {
                                 screen[b]='u';
                         }
@@ -383,12 +387,12 @@ int option()
 			screen[b]=' ';
                         {
                         b+=1;
-						}
-				}
+			}
+		}
 				screen[30]='\n';
                 a+=1;
                 printf("%s",screen);
-		}				
+	}				
 	
 
 	 return 0;
